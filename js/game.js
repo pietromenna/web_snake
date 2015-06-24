810a13bf7ce91de7fcdc435021a837044699a86d
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-24 11:20:03 -0400

    Draw a small snake, now using tick function

--- js/game.js
+++ js/game.js
@@ -1,17 +1,19 @@
+// Any drawable point
+
 var Point = function(x, y) {
-    return {x, y};
+    if (x >= 0 && x <= 60 && y >= 0 && y <= 60)
+        return {x, y};
+    else {
+        throw 'Invalid point';
+    }
 }
 
 function Game(){
-    var snake = [];
-    var direction = '';
+    this.snake = [new Point(30,30),new Point(31,30),new Point(32,30), new Point(33,30) ];
+    var direction = 'LEFT';
     var food = [];
     var score = 0;
 }
-
-Game.prototype.tick = function() {
-    
-};
 
 function clearCanvas() {
     canvas = document.getElementById("canvas");
@@ -29,8 +31,16 @@ function drawPoint(point, color) {
     canvas_width = canvas.width;
     var x_size = canvas_width / 60;
     var y_size = canvas_height / 60;
-    var x_offset = point.x;
-    var y_offset = point.y;
+    var x_offset = point.x * x_size;
+    var y_offset = point.y * y_size;
     context.fillStyle = color;
     context.fillRect(x_offset,y_offset,x_size,y_size);
 }
+
+Game.prototype.tick = function() {
+    clearCanvas();
+    // Draw the snake
+    for (var i=0; i < this.snake.length; i++){
+        drawPoint(this.snake[i],"green");
+    }
+};
